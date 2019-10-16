10ca26c56b72ebf651e90025301b2a82f8e62822
Grzegorz <grzesiektomaka@gmail.com>
2019-10-16 23:42:36 +0200

    userSchema/ signin/ signup/ signout

---
+++ controllers/user.js
@@ -0,0 +1,52 @@
+const User = require("../models/user");
+const jwt = require("jsonwebtoken"); //to generate signed token
+const expressJwt = require("express-jwt"); //for authorization check 
+const { errorHandler } = require("../helpers/dbErrorHandler.js");
+
+exports.signup = (req, res) => {
+    // console.log("req.body", req.body);
+    const user = new User(req.body);
+    user.save((err, user) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        user.salt = undefined;
+        user.hashed_password = undefined;
+        res.json({
+            user
+        });
+    });
+};
+
+exports.signin = (req, res) => {
+    const { email, password} = req.body;
+    User.findOne({email}, (err, user) => {
+        if(err || !user){
+            return res.status(400).json({
+                err: "User with that email does not exist"
+            });
+        }
+        //authenticate method
+        if(!user.authenticate(password)){
+            return res.status(401).json({
+                error: 'Email and password does not match'
+            });
+        }
+
+        //generate a signed token with user id and secret key 
+        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET);
+
+        res.cookie('t', token, {expire: new Date() + 9999});
+        //return response to frontend
+        const { _id, name, email, role} = user;
+        return res.json({token, user: {_id, email, name, role}})
+
+    });
+};
+
+exports.signout = (req, res) => {
+    res.clearCookie("t");
+    res.json({message: "Signout success"});
+}
